8a04620bc37ab2c93d482193c743b4d5a0367aa4
Hannah Neal <hannahmneal@gmail.com>
2019-03-27 02:20:17 -0500

    I have accidentally been pushing to StudentExercise4; set up studentexercise5 project with InstructorController and ExerciseController; tested methods throughout process but commits are on StudentExercise4; need to see about transferring for records.

--- StudentExercise5-WebAPI/Controllers/CohortController.cs
+++ StudentExercise5-WebAPI/Controllers/CohortController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudentExercise5_WebAPI.Models;
 
 namespace StudentExercise5_WebAPI.Controllers
 {
-    public class CohortController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CohortController : ControllerBase
     {
-        public IActionResult Index()
+        // GET: api/Cohort2
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            return View();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Cohort2/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cohort2
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Cohort2/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
--- StudentExercise5-WebAPI/Controllers/ValuesController.cs
+++ StudentExercise5-WebAPI/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StudentExercise5_WebAPI.Models;
+
 
 namespace StudentExercise5_WebAPI.Controllers
 {
