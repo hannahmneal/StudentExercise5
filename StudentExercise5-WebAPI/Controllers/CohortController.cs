06913b688d7184da7d4eb72568340b856b90de83
hanna <hannahmneal@gmail.com>
2019-04-01 04:05:43 -0500

    set up get all Cohorts

--- StudentExercise5-WebAPI/Controllers/CohortController.cs
+++ StudentExercise5-WebAPI/Controllers/CohortController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -12,33 +13,63 @@ namespace StudentExercise5_WebAPI.Controllers
     [ApiController]
     public class CohortController : ControllerBase
     {
-        // GET: api/Cohort2
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public SqlConnection Connection
         {
-            return new string[] { "value1", "value2" };
+            get
+            {
+                string connectionString = "Server=HNEAL-PC\\SQLEXPRESS;Database=StudentExercise3;Integrated Security=true";
+                return new SqlConnection(connectionString);
+            }
         }
 
-        // GET: api/Cohort2/5
+
+        // GET: api/Cohort
+        [HttpGet]
+        public IEnumerable<Cohort> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, CohortName FROM Cohort";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Cohort> cohorts = new List<Cohort>();
+                    while (reader.Read())
+                    {
+                        Cohort cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CohortName = reader.GetString(reader.GetOrdinal("CohortName"))
+                        };
+                        cohorts.Add(cohort);
+                    };
+                    reader.Close();
+                    return cohorts;
+                }
+            }
+        }
+
+        // GET: api/Cohort/id
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Cohort2
+        // POST: api/Cohort
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Cohort2/5
+        // PUT: api/Cohort/id
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE: api/ApiWithActions/id
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
